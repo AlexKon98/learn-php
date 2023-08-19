bfe5160182473beb02297a81b2cc3a829e0e4ec1
AlexKon060698 <aleksander.kononenko2016@yandex.ru>
2023-08-19 22:08:56 +0300

    updated

--- getpost/file.php
+++ getpost/file.php
@@ -6,7 +6,7 @@
   <title>Document</title>
 </head>
 <body>
-  <!-- Обязательно указывать форме атрибут enctype, чтобы отправлять файлы -->
+  <!-- Обязательно указывать форме атрибут enctype="multipart/form-data", чтобы отправлять файлы -->
   <form action="/file.php" method="post" enctype="multipart/form-data">
     <input type="file" name="photo">
     <input type="submit" value="Отправить">
@@ -15,5 +15,16 @@
 </html>
 
 <?php
-var_dump($_FILES);
+if (isset($_FILES['photo'])) {
+  try {
+    move_uploaded_file($_FILES['photo']['tmp_name'], './' . $_FILES['photo']['name']);
+?>
+
+<img src="<?php echo './' . $_FILES['photo']['name'] ?>" alt="">
+
+<?php
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+}
 ?>
